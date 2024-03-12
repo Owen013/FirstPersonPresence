3420ebcbd6c731d554787c091c4602348d719e53
Owen_013 <owen@crawfordsix.com>
2024-03-11 22:07:59 -0400

    fixed arm for cracked simple lanterns; added arms for solanum stones, vision torch, slide reels

--- Immersion/Patches.cs
+++ Immersion/Patches.cs
@@ -45,17 +45,23 @@ public static class Patches
     }
 
     [HarmonyPostfix]
+    [HarmonyPatch(typeof(NomaiConversationStone), nameof(NomaiConversationStone.Awake))]
+    private static void OnSolanumStoneAwake(NomaiConversationStone __instance)
+    {
+        foreach (MeshRenderer renderer in __instance.GetComponentsInChildren<MeshRenderer>())
+        {
+            if (renderer.name.Contains("_Back"))
+            {
+                ToolArmHandler.NewArm(renderer.transform, new Vector3(0.1855f, -0.0942f, -0.184f), Quaternion.Euler(0f, 0f, 340.6367f), new Vector3(0.9f, 0.9f, 0.9f))?.AddComponent<ItemToolArm>();
+            }
+        }
+    }
+
+    [HarmonyPostfix]
     [HarmonyPatch(typeof(ScrollItem), nameof(ScrollItem.Awake))]
     private static void OnScrollAwake(SharedStone __instance)
     {
         ToolArmHandler.NewArm(__instance.transform.Find("Props_NOM_Scroll/Props_NOM_Scroll_Geo/"), new Vector3(-0.1748f, -0.0246f, -0.1213f), Quaternion.Euler(358.7909f, 107.971f, 3.502f), new Vector3(0.9f, 0.9f, 0.9f))?.AddComponent<ItemToolArm>();
-    }
-
-    [HarmonyPostfix]
-    [HarmonyPatch(typeof(DreamLanternItem), nameof(DreamLanternItem.Awake))]
-    private static void OnDreamLanternAwake(DreamLanternItem __instance)
-    {
-        ToolArmHandler.NewArm(__instance.transform.Find("Props_IP_Artifact_ViewModel/artifact_geo"), new Vector3(0.1389f, 0.3851f, -0.144f), Quaternion.Euler(0f, 0f, 90f), new Vector3(1.2f, 1.2f, 1.2f))?.AddComponent<ItemToolArm>();
     }
 
     [HarmonyPostfix]
@@ -68,5 +74,32 @@ public static class Patches
             arm = ToolArmHandler.NewArm(__instance.transform.Find("Props_IP_Lantern_Crack/Lantern_geo"), new Vector3(0.2494f, 0.6859f, 0.0476f), Quaternion.Euler(330f, 320f, 90f), new Vector3(1.2f, 1.2f, 1.2f));
         }
         arm?.AddComponent<ItemToolArm>();
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(SlideReelItem), nameof(SlideReelItem.Awake))]
+    private static void OnSlideReelAwake(SlideReelItem __instance)
+    {
+        foreach (MeshRenderer renderer in __instance.GetComponentsInChildren<MeshRenderer>())
+        {
+            if (renderer.name.Contains("Frame_"))
+            {
+                ToolArmHandler.NewArm(renderer.transform, new Vector3(-0.3298f, 0.121f, 0.133f), Quaternion.Euler(358.9547f, 87.493f, 84.6117f), new Vector3(0.9f, 0.9f, 0.9f))?.AddComponent<ItemToolArm>();
+            }
+        }
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(DreamLanternItem), nameof(DreamLanternItem.Awake))]
+    private static void OnDreamLanternAwake(DreamLanternItem __instance)
+    {
+        ToolArmHandler.NewArm(__instance.transform.Find("Props_IP_Artifact_ViewModel/artifact_geo"), new Vector3(0.1389f, 0.3851f, -0.144f), Quaternion.Euler(0f, 0f, 90f), new Vector3(1.2f, 1.2f, 1.2f))?.AddComponent<ItemToolArm>();
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(VisionTorchItem), nameof(VisionTorchItem.Awake))]
+    private static void OnVisionTorchAwake(VisionTorchItem __instance)
+    {
+        ToolArmHandler.NewArm(__instance.transform.Find("Prefab_IP_VisionTorchProjector/Props_IP_ScannerStaff/Scannerstaff_geo"), new Vector3(0.0403f, 1.0224f, 0.141f), Quaternion.Euler(345.0329f, 184.0765f, 358.0521f), Vector3.one)?.AddComponent<ItemToolArm>();
     }
 }
