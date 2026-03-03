256c330ce5b1467bb758ebe01b041bde17200239
owen <owen@OWENS_DESKTOP>
2026-03-03 04:40:15 -0500

    tool sway now continues decaying in ZeroG instead of just staying where it was

--- Immersion/Scripts/Components/ViewmodelSwayController.cs
+++ Immersion/Scripts/Components/ViewmodelSwayController.cs
@@ -33,34 +33,37 @@ public class ViewmodelSwayController : MonoBehaviour
 
         // only add new sway if player is in ground movement mode and the game is unpaused
         float deltaTime = OWTime.IsPaused(OWTime.PauseType.Reading) ? Time.unscaledDeltaTime : Time.deltaTime;
-        if (deltaTime != 0f && OWInput.IsInputMode(InputMode.Character) && !(PlayerState.InZeroG() && PlayerState.IsWearingSuit()))
+        if (deltaTime != 0f)
         {
-            // get look input
-            Vector2 lookInput = OWInput.GetAxisValue(InputLibrary.look);
-            lookInput *= _cameraController._playerCamera.fieldOfView / _cameraController._initFOV;
-            lookInput *= InputUtil.IsMouseMoveAxis(InputLibrary.look.AxisID) ? 0.01666667f : deltaTime;
-            bool isAlarmWakingPlayer = Locator.GetAlarmSequenceController() != null && Locator.GetAlarmSequenceController().IsAlarmWakingPlayer();
-            if (_cameraController._zoomed || isAlarmWakingPlayer)
-                lookInput *= PlayerCameraController.ZOOM_SCALAR;
-
-            // player can't turn left or right if turning is locked
-            if (_playerController._isTurningLocked)
-                lookInput.x = 0f;
-
-            // horizontal sway is reduced the more up/down player is looking
-            lookInput.x *= (Mathf.Cos(degreesY / 90f * Mathf.PI) + 1f) * 0.5f;
-
-            // cancel out vertical sway if player is at max or min vertical look angle and is trying to turn more in that direction
-            if (degreesY >= PlayerCameraController._maxDegreesYNormal)
-                lookInput.y = Mathf.Min(0f, lookInput.y);
-            if (degreesY <= PlayerCameraController._minDegreesYNormal)
-                lookInput.y = Mathf.Max(0f, lookInput.y);
-
             // decay tool sway
             _currentSway = Vector2.SmoothDamp(_currentSway, Vector2.zero, ref _swayVelocity, 0.2f, Mathf.Infinity, deltaTime);
 
-            // add new tool sway
-            _currentSway += -lookInput * (0.5f * Mathf.Cos(Mathf.PI * _currentSway.magnitude) + 0.5f);
+            if (OWInput.IsInputMode(InputMode.Character) && !(PlayerState.InZeroG() && PlayerState.IsWearingSuit()))
+            {
+                // get look input
+                Vector2 lookInput = OWInput.GetAxisValue(InputLibrary.look);
+                lookInput *= _cameraController._playerCamera.fieldOfView / _cameraController._initFOV;
+                lookInput *= InputUtil.IsMouseMoveAxis(InputLibrary.look.AxisID) ? 0.01666667f : deltaTime;
+                bool isAlarmWakingPlayer = Locator.GetAlarmSequenceController() != null && Locator.GetAlarmSequenceController().IsAlarmWakingPlayer();
+                if (_cameraController._zoomed || isAlarmWakingPlayer)
+                    lookInput *= PlayerCameraController.ZOOM_SCALAR;
+
+                // player can't turn left or right if turning is locked
+                if (_playerController._isTurningLocked)
+                    lookInput.x = 0f;
+
+                // horizontal sway is reduced the more up/down player is looking
+                lookInput.x *= (Mathf.Cos(degreesY / 90f * Mathf.PI) + 1f) * 0.5f;
+
+                // cancel out vertical sway if player is at max or min vertical look angle and is trying to turn more in that direction
+                if (degreesY >= PlayerCameraController._maxDegreesYNormal)
+                    lookInput.y = Mathf.Min(0f, lookInput.y);
+                if (degreesY <= PlayerCameraController._minDegreesYNormal)
+                    lookInput.y = Mathf.Max(0f, lookInput.y);
+
+                // add new tool sway
+                _currentSway += -lookInput * (0.5f * Mathf.Cos(Mathf.PI * _currentSway.magnitude) + 0.5f);
+            }
         }
 
         // x sway is less pronounced the more up/down the player is looking
